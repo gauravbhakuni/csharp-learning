967f1dc80eabaf525e97b959f7b609dd71051f71
gauravbhakuni <gsbhakuni03@gmail.com>
2025-11-27 21:45:48 +0530

    Add Variable project with basic data type examples

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 20;
+            int count = 30;
+            float price = 48.52f;
+            char character = 'a';
+            string name = "john";
+            bool flag = false;
+
+            Console.WriteLine("number = " + number);
+            Console.WriteLine("count = " + count);
+            Console.WriteLine("price = " + price);
+            Console.WriteLine("character = " + character);
+            Console.WriteLine("name = " + name);
+            Console.WriteLine("flag = " + flag);
+        }
+    }
+}
