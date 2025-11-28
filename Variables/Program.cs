8b5595ca2e6e0f44244fa8f0d77f4fcba115b07d
gauravbhakuni <gsbhakuni03@gmail.com>
2025-11-28 19:59:59 +0530

    Update Variables project

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -13,12 +13,26 @@ namespace Variables
             string name = "john";
             bool flag = false;
 
-            Console.WriteLine("number = " + number);
-            Console.WriteLine("count = " + count);
-            Console.WriteLine("price = " + price);
-            Console.WriteLine("character = " + character);
-            Console.WriteLine("name = " + name);
-            Console.WriteLine("flag = " + flag);
+            //Console.WriteLine("number = " + number);
+            //Console.WriteLine("count = " + count);
+            //Console.WriteLine("price = " + price);
+            //Console.WriteLine("character = " + character);
+            //Console.WriteLine("name = " + name);
+            //Console.WriteLine("flag = " + flag);
+
+            Console.WriteLine($"{number}, {count}, {price}, {character}, {name}, {flag}");
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", number, count, price, character, name, flag);
+
+            //type conversion
+            // integer to byte
+            int i = 256;
+            byte b = (byte)i;
+            Console.WriteLine(b);
+
+            // string to integer
+            string s = "123";
+            int j = int.Parse(s);
+            Console.WriteLine(j);
         }
     }
 }
