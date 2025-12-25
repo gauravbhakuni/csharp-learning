f17763dcdb0d41bc2e4e99ef5fac76b8b69d8e9a
gauravbhakuni <gsbhakuni03@gmail.com>
2025-12-25 19:41:25 +0530

    Add Basic interface example

---
+++ BasicInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicInterface
+{
+    interface Basic
+    {
+        void Sum(int a, int b);
+        void Diff(int a, int b);
+    }
+    class Program : Basic
+    {
+        public static void Main(string[] args)
+        {
+            string greet = "hello world";
+            Console.WriteLine(greet);
+
+            Basic b = new Program();
+            b.Sum(2, 3);
+            b.Diff(2, 3);
+        }
+
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public void Diff(int a, int b)
+        {
+            Console.WriteLine(a-b);
+        }
+    }
+}
